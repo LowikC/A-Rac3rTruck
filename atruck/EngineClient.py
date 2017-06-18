a669f93c82eb0600b0e5d101c1eac94d6db33c85
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-18 23:12:15 +0200

    Many new files, to refactor

--- atruck/EngineClient.py
+++ atruck/EngineClient.py
@@ -25,13 +25,14 @@ class EngineClient(object):
 
     def get_command(self, im_bgr, timestamp_s, status):
         logging.debug("Get command client")
+        print(timestamp_s)
         if not self.probe.up():
             raise NoServerException("Server {url} not available".format(url=self.process_url))
 
         cv2.imwrite("/home/robot/test.jpg", im_bgr)
         request_files = self.get_request_files(im_bgr)
         request_data = self.get_request_data(status, timestamp_s)
-        logging.info("Request data: ", request_data)
+        #logging.info("Request data: ", request_data)
         request = post(self.process_url, data=request_data, files=request_files)
         if request.status_code != codes.ok:
             raise FailedRequestException("Response: {r}".format(r=request.text))
@@ -50,7 +51,14 @@ class EngineClient(object):
 
     @staticmethod
     def get_request_data(status, timestamp_s):
-        data = dict()
-        data["image_timestamp_ms"] = int(timestamp_s * 1000)
-        data["status"] = status.to_dict()
+        data = {
+            "image_timestamp_ms": 1111111,
+            "status": {
+                "over": False
+            }
+        }
         return data
+        #data = dict()
+        #data["image_timestamp_ms"] = int(timestamp_s * 1000)
+        #data["status"] = status.to_dict()
+        #return data
