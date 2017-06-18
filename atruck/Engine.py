db263d8db7b693df3038d128b5fa2befee0a6746
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-18 22:41:05 +0200

    Many new files, to refactor

--- atruck/Engine.py
+++ atruck/Engine.py
@@ -1,12 +1,12 @@
 import cv2
 
 
-class EngineClient(object):
+class Engine(object):
     def __init__(self):
         pass
 
     def process(self, im_bgr, timestamp_ms, status):
-        #cv2.imwrite("/home/robot/{tms}.jpg".format(tms=timestamp_ms), im_bgr)
+        cv2.imwrite("/home/robot/{tms}.jpg".format(tms=timestamp_ms), im_bgr)
         cmd = {
             "name": "StraightRun",
             "kwargs": {"speed_rps": 1, "time_ms": 2000}
