0c5dcf9b51e662d50cc7a85444a636b2b0697a84
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-18 22:16:31 +0200

    Many new files, to refactor

--- tests/test_Watches.py
+++ tests/test_Watches.py
@@ -1,15 +1,19 @@
 import time
 from context import atruck
 from atruck.RedButtonWatch import RedButtonWatch
+from atruck.CollisionWatch import CollisionWatch
 from atruck.TruckStatus import TruckStatus
 
 if __name__ == "__main__":
     status = TruckStatus()
     rbw = RedButtonWatch(status)
+    cw = CollisionWatch(status)
     rbw.start()
+    cw.start()
 
     for _ in range(100):
-        print(status.over)
+        print(status.to_dict())
         time.sleep(0.2)
 
-        rbw.join()
+    rbw.join()
+    cw.join()
