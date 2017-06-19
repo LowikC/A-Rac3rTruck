5268901ca31abf21bbb9da97826f37388a9c3e08
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-19 09:50:43 +0200

    Many new files, to refactor

--- atruck/EngineClient.py
+++ atruck/EngineClient.py
@@ -1,6 +1,6 @@
 import cStringIO
 import logging
-import cv2
+import json
 from requests import post, codes
 from PIL import Image
 from ProbeDaemon import ProbeDaemon
@@ -25,15 +25,12 @@ class EngineClient(object):
 
     def get_command(self, im_bgr, timestamp_s, status):
         logging.debug("Get command client")
-        print(timestamp_s)
         if not self.probe.up():
             raise NoServerException("Server {url} not available".format(url=self.process_url))
 
-        cv2.imwrite("/home/robot/test.jpg", im_bgr)
         request_files = self.get_request_files(im_bgr)
         request_data = self.get_request_data(status, timestamp_s)
-        #logging.info("Request data: ", request_data)
-        request = post(self.process_url, data=request_data, files=request_files)
+        request = post(self.process_url, data=json.dumps(request_data), files=request_files)
         if request.status_code != codes.ok:
             raise FailedRequestException("Response: {r}".format(r=request.text))
 
@@ -51,14 +48,7 @@ class EngineClient(object):
 
     @staticmethod
     def get_request_data(status, timestamp_s):
-        data = {
-            "image_timestamp_ms": 1111111,
-            "status": {
-                "over": False
-            }
-        }
+        data = dict()
+        data["image_timestamp_ms"] = int(timestamp_s * 1000)
+        data["status"] = status.to_dict()
         return data
-        #data = dict()
-        #data["image_timestamp_ms"] = int(timestamp_s * 1000)
-        #data["status"] = status.to_dict()
-        #return data
--- tests/test_EngineClient.py
+++ tests/test_EngineClient.py
@@ -5,26 +5,30 @@ This script is used to test upload on the sellit server.
 import cStringIO
 from requests import post
 from PIL import Image
-import cv2
 import json
 import time
+from context import atruck
+from atruck.Camera import Camera
 
 
 if __name__ == '__main__' :
+    cam = Camera(0)
+
     url = "http://lowik.sytes.net"
     port = 53117
 
     url_process = url + ":" + str(port) + "/process"
 
-    im_bgr = cv2.imread("/home/robot/im_1497641529.jpg")
+    im_bgr, ts = cam.next_image()
     im_bgr = Image.fromarray(im_bgr)
     im_buffer = cStringIO.StringIO()
     im_bgr.save(im_buffer, format='JPEG')
     im_buffer.seek(0)
     data = {
-        "image_timestamp_ms": 1111111,
+        "image_timestamp_ms": int(ts * 1000),
         "status": {
-            "over": False
+            "over": False,
+            "collision": False
         }
     }
 
