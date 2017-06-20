718156b4e2c9eaceff7b6b93642921b28de24395
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-20 16:23:03 +0200

    Share command motor

--- atruck/ReadyCommand.py
+++ atruck/ReadyCommand.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from TruckCommand import TruckCommand
 from ev3dev.ev3 import Sound
 
@@ -7,12 +8,14 @@ description = {"name": "ReadyCommand", "kwargs": {}}
 
 class ReadyCommand(TruckCommand):
     def __init__(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.wav = os.path.join(current_dir, "../data/sounds/ready.wav")
         super(ReadyCommand, self).__init__()
 
     def run(self, status):
+        Sound.play(self.wav)
         logging.info("Run ready command")
         status.ready = True
-        Sound.speak("Ready!")
 
     def stop(self):
         pass
