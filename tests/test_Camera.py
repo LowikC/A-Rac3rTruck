5dda9e120e04fdee8032de2dce1f58cf84ba1cf2
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-20 09:40:05 +0200

    New classes, need to refactor commits

--- tests/test_Camera.py
+++ tests/test_Camera.py
@@ -5,10 +5,20 @@ from atruck.Camera import Camera, NoCameraException
 
 
 class TestCamera(unittest.TestCase):
-    def test_resolution(self):
+    def test_resolution_640x480(self):
+        cam = Camera(device_id=0, resolution=(640, 480))
+        im, ts = cam.next_image()
+        self.assertEquals((480, 640, 3), im.shape)
+
+    def test_resolution_320x240(self):
         cam = Camera(device_id=0, resolution=(320, 240))
         im, ts = cam.next_image()
         self.assertEquals((240, 320, 3), im.shape)
+
+    def test_resolution_160x120(self):
+        cam = Camera(device_id=0, resolution=(160, 120))
+        im, ts = cam.next_image()
+        self.assertEquals((120, 160, 3), im.shape)
 
     def test_multiget(self):
         cam = Camera()
