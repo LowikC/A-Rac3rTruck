aec5ccfa937e478dcf16e48eb935f04daf18274c
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-20 17:51:14 +0200

    Add direction calibration

--- atruck/CalibrateDirection.py
+++ atruck/CalibrateDirection.py
@@ -6,7 +6,6 @@ from TruckMotors import direction_motor
 
 class CalibrateDirection(TruckCommand):
     def __init__(self):
-        self.motor = direction_motor
         self.speed_rps = 2
         self.speed_countps = self.motor.count_per_rot * self.speed_rps
         # For unit test
@@ -25,12 +24,12 @@ class CalibrateDirection(TruckCommand):
         time_wait_s = time_ms * 1.8 / 1000
 
         logging.debug("Run direction to the right, until it blocks")
-        self.motor.run_timed(speed_sp=self.speed_countps, time_sp=time_ms)
+        direction_motor.run_timed(speed_sp=self.speed_countps, time_sp=time_ms)
         time.sleep(time_wait_s)
         right_pos = self.motor.position
 
         logging.debug("Run direction to the left, until it blocks")
-        self.motor.run_timed(speed_sp=-self.speed_countps, time_sp=time_ms)
+        direction_motor.run_timed(speed_sp=-self.speed_countps, time_sp=time_ms)
         time.sleep(time_wait_s)
         left_pos = self.motor.position
 
@@ -39,14 +38,14 @@ class CalibrateDirection(TruckCommand):
 
         logging.debug("Run to zero position + an offset to overshoot slightly")
         offset = 10
-        self.motor.run_to_abs_pos(speed_sp=self.speed_countps,
+        direction_motor.run_to_abs_pos(speed_sp=self.speed_countps,
                                   position_sp=self.zero_position + offset)
         time.sleep(time_wait_s)
 
         logging.debug("Run to exact zero position")
-        self.motor.run_to_abs_pos(speed_sp=self.speed_countps/2,
+        direction_motor.run_to_abs_pos(speed_sp=self.speed_countps/2,
                                   position_sp=self.zero_position)
         time.sleep(time_wait_s * 2)
 
         logging.debug("Reset motor, so that current position is 0")
-        self.motor.reset()
+        direction_motor.reset()
