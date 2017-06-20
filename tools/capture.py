e7903e34a0e0932cfbb6bfc4c43e078a407ebbb5
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-20 09:57:20 +0200

    New classes, need to refactor commits

--- tools/capture.py
+++ tools/capture.py
@@ -10,21 +10,21 @@ from atruck.CameraStreamer import CameraStreamer
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Capture images from the camera and store them on disk')
-    parser.add_argument('--save_dir', '-i',
+    parser.add_argument('--save_dir',
                         type=str, default="/home/robot/",
                         help='Directory to save thee images.')
     parser.add_argument('--period_s',
                         type=float, default=1,
                         help='Time between each image (in sec)')
-    parser.add_argument('-n', type=int, default=10,
+    parser.add_argument('--num', type=int, default=10,
                         help='Number of images to save')
-    parser.add_argument('--width', '-w',
+    parser.add_argument('--width',
                         type=int, default=640,
                         help='Width of the images.')
-    parser.add_argument('--height', '-h',
+    parser.add_argument('--height',
                         type=int, default=480,
                         help='Height of the images')
-    parser.add_argument('--device', '-d',
+    parser.add_argument('--device',
                         type=int, default=0,
                         help='Device ID')
     args = parser.parse_args()
