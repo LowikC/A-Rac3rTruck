4de15fcd13cbd1ffed1506dfde801ce80b7182e2
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-20 15:52:29 +0200

    Switch to opencv2 everywhere

---
+++ tools/check_median_hsv_time.py
@@ -0,0 +1,22 @@
+import os
+import cv2
+import time
+from context import atruck
+from atruck.cvision_utils import median_hsv
+
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    im_bgr = cv2.imread(os.path.join(current_dir,
+                                     "../tests/data/GreenFlag_with_flag.jpg"))
+    im_bgr = cv2.resize(im_bgr, (160, 120), interpolation=cv2.INTER_LINEAR)
+
+    for blur_size in [0, 3, 5]:
+        n_loops = 10
+        start_s = time.time()
+        for i in xrange(n_loops):
+            _ = median_hsv(im_bgr, blur_size=5)
+        elapsed_s = (time.time() - start_s) / n_loops
+
+        print("{b} - Average time: {t} [ms]"
+              .format(b=blur_size, t=int(round(elapsed_s * 1000))))
