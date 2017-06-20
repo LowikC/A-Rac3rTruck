51cd9df237640405cdc9bdc6bfbbc0b28465e0d0
Lowik Chanussot <lowikchanussot@gmail.com>
2017-06-20 09:55:00 +0200

    New classes, need to refactor commits

--- tests/test_CameraStreamer.py
+++ tests/test_CameraStreamer.py
@@ -8,13 +8,13 @@ from atruck.CameraStreamer import CameraStreamer
 class TestCameraStreamer(unittest.TestCase):
     def test_start(self):
         queue = Queue(4)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
         time.sleep(2)
         self.assertTrue(queue.empty())
 
     def test_fill_queue(self):
         queue = Queue(4)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
 
         streamer.start()
         time.sleep(2)
@@ -24,7 +24,7 @@ class TestCameraStreamer(unittest.TestCase):
 
     def test_join(self):
         queue = Queue(4)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
 
         streamer.start()
         time.sleep(2)
@@ -39,7 +39,7 @@ class TestCameraStreamer(unittest.TestCase):
 
     def test_queue(self):
         queue = Queue(2)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
 
         streamer.start()
         time.sleep(3)
@@ -52,15 +52,15 @@ class TestCameraStreamer(unittest.TestCase):
 
     def test_full(self):
         queue = Queue(1)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
 
         streamer.start()
         time.sleep(5)
         streamer.join()
 
-    def test_resolution(self):
+    def test_resolution_640x480(self):
         queue = Queue(2)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(640, 480))
 
         streamer.start()
         time.sleep(3)
@@ -69,9 +69,31 @@ class TestCameraStreamer(unittest.TestCase):
         im, _ = queue.get_nowait()
         self.assertEquals((480, 640, 3), im.shape)
 
+    def test_resolution_320x240(self):
+        queue = Queue(2)
+        streamer = CameraStreamer(queue, resolution=(320, 240))
+
+        streamer.start()
+        time.sleep(3)
+        streamer.join()
+
+        im, _ = queue.get_nowait()
+        self.assertEquals((240, 320, 3), im.shape)
+
+    def test_resolution_160x120(self):
+        queue = Queue(2)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
+
+        streamer.start()
+        time.sleep(3)
+        streamer.join()
+
+        im, _ = queue.get_nowait()
+        self.assertEquals((120, 160, 3), im.shape)
+
     def test_timestamp(self):
         queue = Queue(4)
-        streamer = CameraStreamer(queue)
+        streamer = CameraStreamer(queue, resolution=(160, 120))
 
         start_time = time.time()
         streamer.start()
